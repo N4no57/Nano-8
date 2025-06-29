05190a944fdf07a277403097c4697d4d328a7b02
NanoDev <110634754+N4no57@users.noreply.github.com>
2025-06-29 23:12:44 +0100

    added readByte() and writeByte() functions

--- emulator/src/memory.c
+++ emulator/src/memory.c
@@ -1,5 +1,14 @@
 #include "../include/memory.h"
+#include <string.h>
 
 void memory_init(Memory *memory) {
+    memset(memory, 0, sizeof(Memory));
+}
 
+uint8_t readByte(Memory *memory, uint16_t address) {
+    return memory->data[address];
+}
+
+void writeByte(Memory *memory, uint16_t address, uint8_t data) {
+    memory->data[address] = data;
 }
