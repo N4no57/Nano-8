515b2b948e3c5bebdbdbaa02cf397ad7b7b295c8
NanoDev <b.rego.oliveira@gmail.com>
2025-09-26 23:31:14 +0100

    experimenting with ADD instruction

--- emulator/src/main.c
+++ emulator/src/main.c
@@ -4,10 +4,18 @@ int main() {
   CPU cpu;
   CPU_init(&cpu);
 
-  cpu.memory.data[0xFFF0] = MOV_REG_IMM;
-  cpu.memory.data[0xFFF1] = 0x00;
-  cpu.memory.data[0xFFF2] = 32;
-  cpu.memory.data[0xFFF3] = 0x03; // HLT
+  cpu.memory.data[0xFFF0] = 0x00; // reset vec
+  cpu.memory.data[0xFFF1] = 0x10;
+
+  reset(&cpu);
+
+  // inline program injection
+  cpu.memory.data[0x1000] = ADD_REG_REG;
+  cpu.memory.data[0x1001] = 0x01;
+  // end - inline program injection
+
+  cpu.R0 = 0x7F;
+  cpu.R1 = 0x01;
 
   execute(&cpu);
 
