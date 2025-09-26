d2e9c8e994851554731e03ec39a4e8aa6c4e6bef
NanoDev <b.rego.oliveira@gmail.com>
2025-09-26 19:08:04 +0100

    run test code in CPU

--- emulator/src/main.c
+++ emulator/src/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "../include/cpu.h"
 
 int main() {
-  printf("Hello World\nFrom emulator\n");
+  CPU cpu;
+  CPU_init(&cpu);
+
+  cpu.memory.data[0xFFF0] = MOV_REG_IMM;
+  cpu.memory.data[0xFFF1] = 0x00;
+  cpu.memory.data[0xFFF2] = 32;
+  cpu.memory.data[0xFFF3] = 0x03; // HLT
+
+  execute(&cpu);
+
   return 0;
 }
