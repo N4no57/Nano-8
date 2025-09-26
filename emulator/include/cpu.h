516a7c1e1c428e059bf65cab860bc2f5494b43b1
NanoDev <b.rego.oliveira@gmail.com>
2025-09-26 22:50:19 +0100

    added the rest of MOV instruction types to instruction enum

--- emulator/include/cpu.h
+++ emulator/include/cpu.h
@@ -10,6 +10,12 @@
 enum instruction {
     MOV_REG_REG = 0x00,
     MOV_REG_IMM = 0x04,
+    MOV_REG_ABS = 0x08,
+    MOV_REG_IND = 0x0C,
+    MOV_REG_IDX = 0x10,
+    MOV_ABS_REG = 0x14,
+    MOV_IND_REG = 0x18,
+    MOV_IDX_REG = 0x1C,
     HLT = 0x03,
 };
 
