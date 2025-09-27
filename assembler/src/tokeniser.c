760ebb3277f9b41f3933fa555a67b6346ba5f179
NanoDev <b.rego.oliveira@gmail.com>
2025-09-27 19:50:50 +0100

    started work on tokenise()

--- assembler/src/tokeniser.c
+++ assembler/src/tokeniser.c
@@ -29,3 +29,43 @@ void token_list_push(TokenList *token_list, Token T) {
     token_list->count++;
 }
 
+TokenList tokenise(char **lines, SymbolTable *symbol_table) {
+    TokenList token_list = {};
+    int current_line = 0;
+    int buff_index = 0;
+    char buff[MAX_LINE_LENGTH] = {0};
+
+    while (lines[current_line] != NULL) {
+        buff_index = 0;
+        memset(buff, 0, MAX_LINE_LENGTH);
+        for (int i = 0; i < strlen(lines[current_line]); i++) {
+            Token token = {};
+            if (isspace(lines[current_line][i])) {
+                while (isspace(lines[current_line][i])) {
+                    i++;
+                }
+            }
+            buff[buff_index] = lines[current_line][i];
+
+            if (strcmp(buff, "hlt") == 0 && strcmp(buff, "nop") == 0) {
+                token.type = TOKEN_MNEMONIC;
+                token.str_val = strdup(buff);
+                token_list_push(&token_list, token);
+                memset(buff, 0, buff_index + 1);
+                buff_index = -1;
+            }
+
+            if (buff[buff_index] == ':') {
+                token.type = TOKEN_LABEL;
+                token.str_val = strdup(buff);
+                memset(buff, 0, buff_index + 1);
+                buff_index = -1;
+            }
+
+            buff_index++;
+        }
+        current_line++;
+    }
+
+    return token_list;
+}
