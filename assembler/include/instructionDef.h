3d855447366e36a1e8c7823e5b1150ef95e267fa
NanoDev <b.rego.oliveira@gmail.com>
2025-09-27 22:26:27 +0100

    added handlers to get instruction size

--- assembler/include/instructionDef.h
+++ assembler/include/instructionDef.h
@@ -4,18 +4,25 @@
 
 #ifndef INSTRUCTIONDEF_H
 #define INSTRUCTIONDEF_H
+
 #include <stdint.h>
 #include <string.h>
+#include "tokeniser.h"
 
 typedef struct {
     const char *mnemonic;
     uint8_t base_opcode;
     int operand_count;
-    void (*encode)(uint8_t *out, char **operands);
+    void (*encode)(uint8_t base_opcode, int operand_count, uint8_t *out, Token tokens[]);
+    uint16_t (*get_size)(Token tokens[]);
 } InstructionDef;
 
-InstructionDef *find_instruction(InstructionDef *table[], const char *mnemonic);
-void encode_hlt(uint8_t *out, char **operands);
-void encode_nop(uint8_t *out, char **operands);
+InstructionDef *find_instruction(InstructionDef table[], int table_size, const char *mnemonic);
+
+uint16_t get_size_hlt(Token tokens[]);
+uint16_t get_size_nop(Token tokens[]);
+
+void encode_hlt(uint8_t base_opcode, int operand_count, uint8_t *out, Token tokens[]);
+void encode_nop(uint8_t base_opcode, int operand_count, uint8_t *out, Token tokens[]);
 
 #endif //INSTRUCTIONDEF_H
--- assembler/src/instructionDef.c
+++ assembler/src/instructionDef.c
@@ -4,11 +4,38 @@
 
 #include "../include/instructionDef.h"
 
-InstructionDef *find_instruction(InstructionDef *table[], const char *mnemonic) {
-    for (int i = 0; i < sizeof(table) / sizeof(InstructionDef); i++) {
-        if (strcmp(mnemonic, table[i]->mnemonic) == 0) {
-            return table[i];
+#include <stdio.h>
+#include <stdlib.h>
+
+InstructionDef *find_instruction(InstructionDef table[], int table_int, const char *mnemonic) {
+    for (int i = 0; i < table_int; i++) {
+        if (strcmp(mnemonic, table[i].mnemonic) == 0) {
+            return &table[i];
         }
     }
     return NULL;
+}
+
+uint16_t get_size_hlt(Token tokens[]) {
+    // if (tokens != NULL) {
+    //     printf("Unexpected tokens, %s", tokens[0].str_val);
+    //     exit(1);
+    // }
+    return 1;
+}
+
+uint16_t get_size_nop(Token tokens[]) {
+    // if (tokens != NULL) {
+    //     printf("Unexpected tokens, %s", tokens[0].str_val);
+    //     exit(1);
+    // }
+    return 1;
+}
+
+void encode_hlt(const uint8_t base_opcode, int operand_count, uint8_t *out, Token tokens[]) {
+    out[0] = base_opcode;
+}
+
+void encode_nop(const uint8_t base_opcode, int operand_count, uint8_t *out, Token tokens[]) {
+    out[0] = base_opcode;
 }
