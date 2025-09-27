152c233c321d5ddca7ce812c70f826b35a30e67b
NanoDev <b.rego.oliveira@gmail.com>
2025-09-27 23:06:30 +0100

    commented redundant code

--- assembler/src/main.c
+++ assembler/src/main.c
@@ -6,25 +6,38 @@
 
 #include "../include/instructionDef.h"
 #include "../include/symbolTable.h"
+ #include "../include/tokeniser.h"
 
 #define SYMBOL_TABLE_BASE_SIZE 16
 #define MAX_LINE_LENGTH 1024
 #define LINE_NUM_BASE_SIZE 16
 
-enum instruction {
-	MOV_REG_REG = 0x00,
-	MOV_REG_IMM = 0x04,
-	MOV_REG_ABS = 0x08,
-	MOV_REG_IND = 0x0C,
-	MOV_REG_IDX = 0x10,
-	MOV_ABS_REG = 0x14,
-	MOV_IND_REG = 0x18,
-	MOV_IDX_REG = 0x1C,
-	ADD_REG_REG = 0x01,
-	ADD_REG_IMM = 0x05,
-	HLT = 0x03,
-	NOP = 0x13,
-};
+// enum instruction {
+// 	MOV_REG_REG = 0x00,
+// 	MOV_REG_IMM = 0x04,
+// 	MOV_REG_ABS = 0x08,
+// 	MOV_REG_IND = 0x0C,
+// 	MOV_REG_IDX = 0x10,
+// 	MOV_ABS_REG = 0x14,
+// 	MOV_IND_REG = 0x18,
+// 	MOV_IDX_REG = 0x1C,
+// 	ADD_REG_REG = 0x01,
+// 	ADD_REG_IMM = 0x05,
+// 	HLT = 0x03,
+// 	NOP = 0x13,
+// };
+//
+// enum registers {
+// 	R0 = 0x0,
+// 	R1 = 0x1,
+// 	R2 = 0x2,
+// 	R3 = 0x3,
+// 	PC = 0xB,
+// 	SP = 0xC,
+// 	BP = 0xD,
+// 	H = 0xE,
+// 	L = 0xF
+// };
 
 enum registers {
 	R0 = 0x0,
