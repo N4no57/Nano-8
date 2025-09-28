cb3034a781117bdaef17fdee712c49c23e4099df
NanoDev <b.rego.oliveira@gmail.com>
2025-09-28 12:46:42 +0100

    migrated operand_parser to utils.h and utils.c

--- assembler/include/utils.h
+++ assembler/include/utils.h
@@ -5,10 +5,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include "tokeniser.h"
+
 typedef struct {
     enum { IMMEDIATE, REGISTER, ABSOLUTE, RELATIVE, INDIRECT_MEM, INDIRECT_REG, INDEXED_MEM} kind;
     union {
-        int imm;
+        int imm; // can be an actual immediate, resolved register or memory address. interpretation dependent on instruction/operand
         int reg;
         struct {
             int reg_high;
@@ -17,5 +19,16 @@ typedef struct {
         } mem_pair;
     };
 } ParsedOperand;
+
+int get_reg(const char *s);
+
+void consume_token(int *tok_idx, Token *t, const TokenList *tok_list);
+
+int get_base(char c);
+
+int is_base_mod(Token t);
+
+// parses tokens until it reaches a "," then returns the parsed operand
+inline ParsedOperand operand_parser(const TokenList *tokens, SymbolTable *symbol_table, int *tok_idx, Token *current_tok);
 
 #endif //UTILS_H
---
+++ assembler/src/utils.c
@@ -0,0 +1,114 @@
+//
+// Created by brego on 28/09/2025.
+//
+
+#include "../include/utils.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_reg(const char *s) {
+    if (strcmp(s, "r0") == 0) return 0;
+    if (strcmp(s, "r1") == 0) return 1;
+    if (strcmp(s, "r2") == 0) return 2;
+    if (strcmp(s, "r3") == 0) return 3;
+    if (strcmp(s, "pc") == 0) return 12;
+    if (strcmp(s, "sp") == 0) return 13;
+    if (strcmp(s, "bp") == 0) return 14;
+    if (strcmp(s, "h") == 0) return 15;
+    if (strcmp(s, "l") == 0) return 16;
+    return -1;
+}
+
+void consume_token(int *tok_idx, Token *t, const TokenList *tok_list) {
+    if (*tok_idx >= tok_list->count) {
+        printf("Out of tokens\n");
+        exit(1);
+    }
+    *t = tok_list->data[*tok_idx];
+    (*tok_idx)++;
+}
+
+int get_base(char c) {
+    if (c == '$') {
+        return 16;
+    }
+    if (c == '%') {
+        return 2;
+    }
+    return 10;
+}
+
+int is_base_mod(Token t) {
+    if (t.str_val[0] == '$' || t.str_val[0] == '%') {
+        return 1;
+    }
+    return 0;
+}
+
+// parses tokens until it reaches a "," then returns the parsed operand
+ParsedOperand operand_parser(const TokenList *tokens, SymbolTable *symbol_table, int *tok_idx, Token *current_tok) {
+    ParsedOperand operand = {0, {0}};
+
+    while (current_tok->type != TOKEN_EOF && current_tok->type != TOKEN_MNEMONIC &&
+        !(current_tok->type == TOKEN_SYMBOL && current_tok->str_val[0] == ',')) {
+        // go through and find out what argument this is and if it is part of a bigger piece or a standalone
+        // e.g. R0 -> enum REGISTER, #1023 -> enum IMMEDIATE and so on
+        switch (current_tok->type) {
+            // most verbose case. needs careful handling.
+            case TOKEN_SYMBOL:
+                if (current_tok->str_val[0] == '(') { // indirect reg/mem
+                    consume_token(tok_idx, current_tok, tokens); // consume "("
+                    if (current_tok->type == TOKEN_REGISTER) { // expect another reg after a ","
+                        operand.kind = INDIRECT_REG;
+                        int reg = 0;
+                        reg = get_reg(current_tok->str_val);
+                        if (reg == -1) {
+                            printf("Invalid register\n");
+                            exit(1);
+                        }
+                        operand.mem_pair.reg_high = reg;
+                        consume_token(tok_idx, current_tok, tokens);
+                        consume_token(tok_idx, current_tok, tokens);
+                        reg = get_reg(current_tok->str_val);
+                        if (reg == -1) {
+                            printf("Invalid register\n");
+                            exit(1);
+                        }
+                        operand.mem_pair.reg_low = reg;
+                        consume_token(tok_idx, current_tok, tokens);
+                        consume_token(tok_idx, current_tok, tokens);
+                    }
+                } else if (is_base_mod(*current_tok)) { // check if base modifier succeeded by a number token
+                    operand.kind = ABSOLUTE;
+                    consume_token(tok_idx, current_tok, tokens); // next token expected to be number token
+                    if (current_tok->type != TOKEN_NUMBER) {
+                        printf("Invalid token\n");
+                        exit(1);
+                    }
+                    // is assumed to already have base converted
+                    operand.imm = current_tok->int_value;
+                    consume_token(tok_idx, current_tok, tokens);
+                } else if (current_tok->str_val[0] == '#') { // check if immediate value
+                    // may be succeeded by base modifier
+
+                }
+            break;
+            case TOKEN_REGISTER:
+                operand.kind = REGISTER;
+            int reg = get_reg(current_tok->str_val);
+            if (reg == -1) {
+                printf("Invalid register\n");
+                exit(1);
+            }
+            operand.reg = reg;
+            consume_token(tok_idx, current_tok, tokens);
+            break;
+            case TOKEN_NUMBER:
+                break;
+            default: break;
+        }
+        }
+
+    return operand;
+}
