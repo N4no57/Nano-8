5f1a7250d5553179bfae639c56d861b1b72bb29c
Bernardo <b.rego.oliveira@gmail.com>
2025-09-28 01:01:47 +0100

    started work on mov instruction

--- assembler/include/instructionDef.h
+++ assembler/include/instructionDef.h
@@ -8,21 +8,24 @@
 #include <stdint.h>
 #include <string.h>
 #include "tokeniser.h"
+#include "utils.h"
 
 typedef struct {
     const char *mnemonic;
     uint8_t base_opcode;
     int operand_count;
-    void (*encode)(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, Token tokens[]);
-    uint16_t (*get_size)(Token tokens[], int operand_count, int expected_operand_count);
+    void (*encode)(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]);
+    uint16_t (*get_size)(int operand_count, int expected_operand_count, ParsedOperand operands[]);
 } InstructionDef;
 
 InstructionDef *find_instruction(InstructionDef table[], int table_size, const char *mnemonic);
 
-uint16_t get_size_hlt(Token tokens[], int operand_count, int expected_operand_count);
-uint16_t get_size_nop(Token tokens[], int operand_count, int expected_operand_count);
+uint16_t get_size_mov(int operand_count, int expected_operand_count, ParsedOperand operands[]);
+uint16_t get_size_hlt(int operand_count, int expected_operand_count, ParsedOperand operands[]);
+uint16_t get_size_nop(int operand_count, int expected_operand_count, ParsedOperand operands[]);
 
-void encode_hlt(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, Token tokens[]);
-void encode_nop(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, Token tokens[]);
+void encode_mov(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]);
+void encode_hlt(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]);
+void encode_nop(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]);
 
 #endif //INSTRUCTIONDEF_H
--- assembler/src/instructionDef.c
+++ assembler/src/instructionDef.c
@@ -16,7 +16,40 @@ InstructionDef *find_instruction(InstructionDef table[], int table_int, const ch
     return NULL;
 }
 
-uint16_t get_size_hlt(Token tokens[], int operand_count, int expected_operand_count) {
+uint16_t get_size_mov(int operand_count, int expected_operand_count, ParsedOperand operands[]) {
+    if (operand_count != expected_operand_count) {
+        fprintf(stderr, "Error: operand count mismatch\n");
+        exit(1);
+    }
+
+    if (operands[0].kind == REGISTER && operands[1].kind == REGISTER) {
+        return 2;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == IMMEDIATE) {
+        return 3;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == ABSOLUTE) {
+        return 4;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == INDIRECT_REG) {
+        return 3;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == INDEXED_MEM) {
+        return 5;
+    }
+    if (operands[0].kind == ABSOLUTE && operands[1].kind == REGISTER) {
+        return 4;
+    }
+    if (operands[0].kind == INDIRECT_REG && operands[1].kind == REGISTER) {
+        return 3;
+    }
+    if (operands[0].kind == INDEXED_MEM && operands[1].kind == REGISTER) {
+        return 5;
+    }
+    // no more possible operand combinations if somehow skip past all if statements something is wrong...
+}
+
+uint16_t get_size_hlt(int operand_count, int expected_operand_count, ParsedOperand operands[]) {
     if (operand_count != expected_operand_count) {
         fprintf(stderr, "Error: operand count mismatch\n");
         exit(1);
@@ -24,7 +57,7 @@ uint16_t get_size_hlt(Token tokens[], int operand_count, int expected_operand_co
     return 1;
 }
 
-uint16_t get_size_nop(Token tokens[], int operand_count, int expected_operand_count) {
+uint16_t get_size_nop(int operand_count, int expected_operand_count, ParsedOperand operands[]) {
     if (operand_count != expected_operand_count) {
         fprintf(stderr, "Error: operand count mismatch\n");
         exit(1);
@@ -32,10 +65,42 @@ uint16_t get_size_nop(Token tokens[], int operand_count, int expected_operand_co
     return 1;
 }
 
-void encode_hlt(const uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, Token tokens[]) {
+void encode_mov(uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]) {
+    if (operands[0].kind == REGISTER && operands[1].kind == REGISTER) {
+        out[(*binary_index)++] = base_opcode;
+        out[(*binary_index)++] = (operands[0].reg << 4) + operands[1].reg;
+        return;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == IMMEDIATE) {
+        out[(*binary_index)++] = base_opcode + 0b00000100;
+        out[(*binary_index)++] = operands[0].reg << 4;
+        out[(*binary_index)++] = (uint8_t)operands[1].imm;
+        return;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == ABSOLUTE) { // TODO
+        return;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == INDIRECT_REG) { // TODO
+        return;
+    }
+    if (operands[0].kind == REGISTER && operands[1].kind == INDEXED_MEM) { // TODO
+        return;
+    }
+    if (operands[0].kind == ABSOLUTE && operands[1].kind == REGISTER) { // TODO
+        return;
+    }
+    if (operands[0].kind == INDIRECT_REG && operands[1].kind == REGISTER) { // TODO
+        return;
+    }
+    if (operands[0].kind == INDEXED_MEM && operands[1].kind == REGISTER) { // TODO
+        return;
+    }
+}
+
+void encode_hlt(const uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]) {
     out[(*binary_index)++] = base_opcode;
 }
 
-void encode_nop(const uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, Token tokens[]) {
+void encode_nop(const uint8_t base_opcode, int operand_count, uint8_t *out, uint16_t *binary_index, ParsedOperand operands[]) {
     out[(*binary_index)++] = base_opcode;
 }
