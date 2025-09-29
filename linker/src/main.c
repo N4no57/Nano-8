da34714334905788f1d905d07e484718150f6db9
Bernardo <b.rego.oliveira@gmail.com>
2025-09-29 12:20:26 +0100

    started with CLI options

--- linker/src/main.c
+++ linker/src/main.c
@@ -1,3 +1,34 @@
-int main() {
-    return 0;
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#define DEFAULT_FILENAME "o.bin"
+
+int main(int argc, char **argv) {
+    char *output = DEFAULT_FILENAME;
+
+    static struct option long_options[] = {
+        {NULL, 0, 0, 0}
+    };
+
+    opterr = 0;
+
+    int option;
+    int option_index = 0;
+    while ((option = getopt_long(argc, argv, "ho:", long_options, &option_index)) != -1) {
+        switch (option) {
+            case 'h':
+                printf("Usage: nano8-ld [options]\n");
+                printf("Options:\n");
+                printf("  -h                 Display this help screen\n");
+				printf("  -o <file>          Place the output into <file>\n");
+                return 0;
+            case 'o':
+                output = optarg;
+                break;
+            default:
+                break;
+        }
+    }
 }
