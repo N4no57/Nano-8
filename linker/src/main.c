b64442ae161ef918fd85e0e22843f26373375085
Bernardo <b.rego.oliveira@gmail.com>
2025-10-02 11:13:07 +0100

    added file input functionality

--- linker/src/main.c
+++ linker/src/main.c
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <stdlib.h>
 
 #define DEFAULT_FILENAME "o.bin"
 #define LINKER_VERSION "1.0.0"
 
 bool configFile = false;
 
-int main(int argc, char **argv) {
+int main(const int argc, char **argv) {
     struct MemoryRegion *a = NULL;
     struct SegmentRule *b = NULL;
 
@@ -56,10 +57,23 @@ int main(int argc, char **argv) {
         }
     }
 
-    const struct ObjectFile obj = readObjectFile("test.o");
-    dumpObjectFile(&obj);
+    const size_t num_files = argc - optind;
 
-    linker(&obj, 1, output, a, b);
+    struct ObjectFile *o = malloc(num_files * sizeof(struct ObjectFile));
 
+    if (num_files == 0) {
+        printf("nano8-ld: fatal-error: no input files\n");
+        printf("linking terminated");
+        free(o);
+        return 1;
+    }
+
+    for (int i = optind; i < argc; i++) {
+        o[i-optind] = readObjectFile(argv[i]);
+    }
+
+    linker(o, num_files, output, a, b);
+
+    free(o);
     return 0;
 }
