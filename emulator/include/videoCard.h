bc1c0098f2163bfa0020015248f62adfe5a8122a
NanoDev <b.rego.oliveira@gmail.com>
2025-10-05 18:52:00 +0100

    started work on a new I/O device

---
+++ emulator/include/videoCard.h
@@ -0,0 +1,8 @@
+//
+// Created by brego on 05/10/2025.
+//
+
+#ifndef VIDEOCARD_H
+#define VIDEOCARD_H
+
+#endif //VIDEOCARD_H
---
+++ emulator/src/videoCard.c
@@ -0,0 +1,5 @@
+//
+// Created by brego on 05/10/2025.
+//
+
+#include "videoCard.h"
