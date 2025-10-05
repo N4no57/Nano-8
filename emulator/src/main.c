32d49b79484f9470c1c7e132f73d6665c334a159
NanoDev <b.rego.oliveira@gmail.com>
2025-10-05 18:51:35 +0100

    loaded a rom image into memory instead

--- emulator/src/main.c
+++ emulator/src/main.c
@@ -5,7 +5,7 @@
 #include "../include/emulatorTests.h"
 
 int main() {
-    test_emulator();
+    //test_emulator();
 
     CPU cpu;
     CPU_init(&cpu);
@@ -22,20 +22,19 @@ int main() {
     disk.sectors = 18;
     disk.tracks = 80;
     disk.heads = 2;
+    disk.filename = "floppy.img";
     disk.size = disk.sector_size * disk.sectors * disk.tracks * disk.heads;
     disk.data = malloc(disk.size);
     memset(disk.data, 0, disk.size);
-    memset(disk.data, 0xFF, 512);
 
     mount_floppy_disk(&cpu.floppy_controller, &disk, 0);
 
-    cpu.memory.data[0xFFF0] = 0x00; // reset vec
-    cpu.memory.data[0xFFF1] = 0x80;
+
+
+    FILE *f = fopen("rombin.bin", "rb");
+    fread(&cpu.memory.data[0x8000], 1, 0xFFFF-0x8000, f);
 
     reset(&cpu);
-
-    FILE *f = fopen("o.bin", "rb");
-    fread(&cpu.memory.data[0x8000], 1, 512, f);
 
     while (1) {
         tick_fdc(&cpu.floppy_controller);
